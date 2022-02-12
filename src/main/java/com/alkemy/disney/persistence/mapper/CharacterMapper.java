82734acf3ba7003b9cb841843f9a530e02d09a42
feltom1000 <feltom1000@hotmail.com>
2022-02-12 12:56:24 -0300

    add: CharacterMapper done

---
+++ src/main/java/com/alkemy/disney/persistence/mapper/CharacterMapper.java
@@ -0,0 +1,28 @@
+package com.alkemy.disney.persistence.mapper;
+
+import com.alkemy.disney.persistence.entity.Personaje;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CharacterMapper {
+
+    @Mappings({
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "imagen", target = "img"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "edad", target = "age"),
+            @Mapping(source = "peso", target = "weight"),
+            @Mapping(source = "historia", target = "story"),
+            @Mapping(source = "peliculasOSeriesAsociadas", target = "moviesOrSeriesList")
+    })
+    Character toCharacter(Personaje personaje);
+    List<Character> toCharacters(List<Personaje> personajes);
+
+    @InheritInverseConfiguration
+    Personaje toPersonaje(Character character);
+}
