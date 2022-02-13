e3941d6b1ac3972e3c800f1dec1544ec9f5ca355
feltom1000 <feltom1000@hotmail.com>
2022-02-13 15:13:58 -0300

    QueryMethods in PeliculaOSerieCrudRepository

--- src/main/java/com/alkemy/disney/persistence/crud/PeliculaOSerieCrudRepository.java
+++ src/main/java/com/alkemy/disney/persistence/crud/PeliculaOSerieCrudRepository.java
@@ -2,46 +2,37 @@ package com.alkemy.disney.persistence.crud;
 
 import com.alkemy.disney.domain.MovieOrSerie;
 import com.alkemy.disney.persistence.entity.PeliculaOSerie;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface PeliculaOSerieCrudRepository extends CrudRepository<PeliculaOSerie, Integer> {
-    Optional<List<MovieOrSerie>> getByGeneroId(int genreId);
-    Optional<List<MovieOrSerie>> getBy
+    Optional<List<MovieOrSerie>> findByGeneroId(int genreId);
+    Optional<List<MovieOrSerie>> findByTitulo(String titulo);
+    Optional<List<MovieOrSerie>> findByCalificacion(int calificacion);
+    Optional<List<MovieOrSerie>> findByFechaDeCreacion(Date fecha);
+
+    @Modifying
+    @Query(value = "UPDATE pelicula_serie u SET u.imagen = ?1 WHERE u.id = ?2", nativeQuery = true)
+    PeliculaOSerie editImagenById(String img, int id);
+
+    @Modifying
+    @Query(value = "UPDATE pelicula_serie u SET u.titulo = ?1 WHERE u.id = ?2", nativeQuery = true)
+    PeliculaOSerie editTituloById(String titulo, int id);
+
+    @Modifying
+    @Query(value = "UPDATE pelicula_serie u SET u.fecha_creacion = ?1 WHERE u.id = ?2", nativeQuery = true)
+    PeliculaOSerie editFechaDeCreacionById(Date fecha, int id);
+
+    @Modifying
+    @Query(value = "UPDATE pelicula_serie u SET u.calificacion = ?1 WHERE u.id = ?2", nativeQuery = true)
+    PeliculaOSerie editCalificacionById(int calificacion, int id);
+
+    @Modifying
+    @Query(value = "UPDATE pelicula_serie u SET u.id_genero = ?1 WHERE u.id = ?2", nativeQuery = true)
+    PeliculaOSerie editIdGeneroById(int idGenero, int id);
 }
-
-//    List<MovieOrSerie> getAll();
-//    Optional<List<MovieOrSerie>> getByGenre(int genreId);
-//    Optional<List<MovieOrSerie>> getByTitle(String title);
-//    Optional<List<MovieOrSerie>> getByCalification(int calification);
-//    Optional<List<MovieOrSerie>> getByCreationDate(Date creationDate);
-//    Optional<MovieOrSerie> getMovieOrSeriesById(int Id);
-//    MovieOrSerie save(MovieOrSerie movieOrSerie);
-//    MovieOrSerie edit(MovieOrSerie movieOrSerie);
-//    void delete(int movieOrSeriesId);
-
-//Optional<List<Personaje>> findByNombre(String nombre);
-//    Optional<List<Personaje>> findByEdad(int edad);
-//    Optional<List<Personaje>> findByPeliculasOSeriesAsociadas(MovieOrSerie movieOrSerie);
-//
-//    @Modifying
-//    @Query(value = "UPDATE personaje u SET u.imagen = ?1 WHERE u.id = ?2", nativeQuery = true)
-//    Personaje editImagenById(String img, int id);
-//
-//    @Modifying
-//    @Query(value = "UPDATE personaje u SET u.nombre = ?1 WHERE u.id = ?2", nativeQuery = true)
-//    Personaje editNombreById(String nombre, int id);
-//
-//    @Modifying
-//    @Query(value = "UPDATE personaje u SET u.edad = ?1 WHERE u.id = ?2", nativeQuery = true)
-//    Personaje editEdadById(Integer edad, int id);
-//
-//    @Modifying
-//    @Query(value = "UPDATE personaje u SET u.peso = ?1 WHERE u.id = ?2", nativeQuery = true)
-//    Personaje editPesoById(Integer peso, int id);
-//
-//    @Modifying
-//    @Query(value = "UPDATE personaje u SET u.historia = ?1 WHERE u.id = ?2", nativeQuery = true)
-//    Personaje editHistoriaById(String historia, int id);
